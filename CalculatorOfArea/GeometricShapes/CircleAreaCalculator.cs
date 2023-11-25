7b500164bcd9f12f5de3e88451fd53a5ae8e646f
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-11-25 19:11:35 +0300

    created CircleAreaCalculator and TriangleAreaCalculator

---
+++ CalculatorOfArea/GeometricShapes/CircleAreaCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace CalculatorOfArea.GeometricShapes
+{
+    /// <summary>
+    /// Implementation of the Circle area calculator.
+    /// </summary>
+    /// /// <param name="radius">The radius number.</param>
+    /// <returns>Circle area.</returns>
+    public class CircleAreaCalculator : IAreaCalculator
+    {
+        private double radius;
+
+        public CircleAreaCalculator(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
---
+++ CalculatorOfArea/GeometricShapes/TriangleAreaCalculator.cs
@@ -0,0 +1,51 @@
+﻿namespace CalculatorOfArea.GeometricShapes
+{
+    /// <summary>
+    /// Implementation of the Triangle area calculator.
+    /// </summary>
+    /// <param name="sideA">The sideA number.</param>
+    /// <param name="sideB">The sideB number.</param>
+    /// <param name="sideC">The sideC number.</param>
+    /// <returns>Triangle area.</returns>
+    public class TriangleAreaCalculator : IAreaCalculator
+    {
+        private double sideA, sideB, sideC;
+
+        public TriangleAreaCalculator(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        /// <summary>
+        /// Implementation of the triangle area calculatr
+        /// </summary>
+        /// <returns>Value of area</returns>
+        public double CalculateArea()
+        {
+            if (IsRightAngled())
+            {
+
+                return sideA * sideB / 2;
+            }
+            else
+            {
+                double s = (sideA + sideB + sideC) / 2;
+                return Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+            }
+
+        }
+
+        /// <summary>
+        /// Implementation of checking whether a triangle is rectangular
+        /// </summary>
+        /// <returns>returns True if the triangle is rectangular</returns>
+        private bool IsRightAngled()
+        {
+            return (sideA * sideA + sideB * sideB == sideC * sideC) ||
+               (sideA * sideA + sideC * sideC == sideB * sideB) ||
+               (sideB * sideB + sideC * sideC == sideA * sideA);
+        }
+    }
+}
