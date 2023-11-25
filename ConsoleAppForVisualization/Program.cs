fc3071afa72399980b1ced2bdfc51c9206060925
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-11-25 21:32:21 +0300

    Created ConsoleApp

---
+++ ConsoleAppForVisualization/Program.cs
@@ -0,0 +1,7 @@
+﻿using CalculatorOfArea;
+
+IAreaCalculatorFactory Geometry = new AreaCalculatorFactory();
+IAreaCalculator circleCalculator = Geometry.Create(new double[] { 5 });
+IAreaCalculator triangleCalculator = Geometry.Create(new double[] { 3, 4, 5 });
+Console.WriteLine($"Площадь круга: {circleCalculator.CalculateArea()}");
+Console.WriteLine($"Площадь треугольника: {triangleCalculator.CalculateArea()}");
